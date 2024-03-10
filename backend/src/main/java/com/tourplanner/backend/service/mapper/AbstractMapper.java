9aeec396a1e9e52a707151c785e9a84a5194d4b0
Yehor Zudikhin <yehorzudikhin17@gmail.com>
2024-03-10 16:47:13 +0100

    Models & DTOs configured, DB connection established

---
+++ backend/src/main/java/com/tourplanner/backend/service/mapper/AbstractMapper.java
@@ -0,0 +1,15 @@
+package com.tourplanner.backend.service.mapper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public abstract class AbstractMapper<S, T> implements Mapper<S, T> {
+    public List<T> mapToDto(Collection<S> source) {
+        List<T> targets = new ArrayList<>();
+        source.forEach(s -> {
+            targets.add(mapToDto(s));
+        });
+        return targets;
+    }
+}
---
+++ backend/src/main/java/com/tourplanner/backend/service/mapper/TourLogMapper.java
@@ -0,0 +1,22 @@
+package com.tourplanner.backend.service.mapper;
+
+import com.tourplanner.backend.model.TourLog;
+import com.tourplanner.backend.service.dto.TourLogDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TourLogMapper extends AbstractMapper<TourLog, TourLogDTO> {
+    @Override
+    public TourLogDTO mapToDto(TourLog sourceTourLog) {
+        return TourLogDTO.builder()
+                .id(sourceTourLog.getId())
+                .dateTime(sourceTourLog.getDateTime())
+                .comment(sourceTourLog.getComment())
+                .difficulty(sourceTourLog.getDifficulty())
+                .distance(sourceTourLog.getDistance())
+                .totalTime(sourceTourLog.getTotalTime())
+                .rating(sourceTourLog.getRating())
+                .tourId(sourceTourLog.getTour().getId())
+                .build();
+    }
+}
